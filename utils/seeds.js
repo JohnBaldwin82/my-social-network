341a162e8fe138475bf22092e32301723b4a24fa
John Baldwin <johnbaldwin82@gmail.com>
2023-08-05 21:59:06 -0600

    last one

--- utils/seeds.js
+++ utils/seeds.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const myData = require('../config/connection')
-const {User, Thought} = require('..models')
+const {myUser, myThought} = require('..models')
 const {getSomeUsers, getSomeThoughts} = require('./data')
 
 myData.once('open', async () => {
@@ -8,8 +8,8 @@ myData.once('open', async () => {
         await User.deletMany({})
         await Thought.deleteMany({})
 
-        const user = await User.create(getSomeUsers(8))
-        const thought = await Thought.create(getSomeThoughts(user, 16))
+        const myUser = await myUser.create(getSomeUsers(8))
+        const myThought = await myThought.create(getSomeThoughts(user, 16))
 
         console.log('Finished')
         process.exit(0)
