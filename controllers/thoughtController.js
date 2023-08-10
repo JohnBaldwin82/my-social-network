6b466abfbbcfb796e74a956f1fa6b7eb91aa191f
John Baldwin <johnbaldwin82@gmail.com>
2023-08-10 14:43:08 -0600

    remove thought now working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -49,17 +49,16 @@ module.exports = {
         thoughtText: req.body.thoughtText,
         username: req.body.username
       },
-      { new: true },
-      (err, result) => {
-        if (result) {
-          res.status(200).json(result);
-          console.log(`Updated: ${result}`);
-        } else {
-          console.log(err);
-          res.status(500).json({ message: 'error', err });
-        }
+      { new: true }
+    ).then((thought) => {
+      if (thought) {
+        res.status(200).json(thought);
+        //console.log(`Updated: ${thought}`);
       }
-    )
+    }).catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
   },
 
   removeThought(req, res) {
@@ -73,13 +72,17 @@ module.exports = {
             { new: true }
           )
       )
-      .then((user) =>
-        !user
-          ? res.status(404).json({
+      .then((user) => {
+        console.log(user);
+        if(user.thoughts.includes(req.params.thoughtId)){
+          res.status(500).json({
             message: 'Error deleting this thought',
           })
-          : res.json({ message: 'Thought successfully removed' })
-      )
+        }
+        else{
+          res.json({ message: 'Thought successfully removed' })
+        }
+      })
       .catch((err) => res.status(500).json(err));
   },
 
